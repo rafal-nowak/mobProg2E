4b7dd23e86619bd73082cb13c276d3ae51881720
rafal-nowak <contact@rafal-nowak.com>
2023-11-14 11:41:15 +0100

    added result file

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 if __name__ == '__main__':
-    with open("input.txt", "r") as data:
+    with open("input.txt", "r") as data, open("result.txt", "w") as result:
         lines = data.readlines()
         temperatures = []
         for line in lines:
             temperature = float(line.strip())
             temperatures.append(temperature)
-        print(lines)
-        print(temperatures)
+        # print(lines)
+        # print(temperatures)
+        result.write("Celsius; Kelvin; Fahrenheit\n")
