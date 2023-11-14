953a4cfb5c4d5dc456e0589e9d83a74e8d83536f
rafal-nowak <contact@rafal-nowak.com>
2023-11-14 11:47:35 +0100

    added conversions

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+def celsius_to_fahrenheit(temperature):
+    return temperature * 1.8 + 32
+
+
+def celsius_to_kelvin(temperature):
+    return temperature + 273.15
+
+
 if __name__ == '__main__':
     with open("input.txt", "r") as data, open("result.txt", "w") as result:
         lines = data.readlines()
@@ -8,3 +16,5 @@ if __name__ == '__main__':
         # print(lines)
         # print(temperatures)
         result.write("Celsius; Kelvin; Fahrenheit\n")
+        for temperature in temperatures:
+            result.write(f"{temperature}; {round(celsius_to_kelvin(temperature), 2)}; {round(celsius_to_fahrenheit(temperature), 2)}\n")
