0940aa32ae542374650fc3344b1fcb7ef4da69f4
rafal-nowak <contact@rafal-nowak.com>
2023-11-14 11:38:57 +0100

    convert strings to floats

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 if __name__ == '__main__':
     with open("input.txt", "r") as data:
         lines = data.readlines()
+        temperatures = []
+        for line in lines:
+            temperature = float(line.strip())
+            temperatures.append(temperature)
         print(lines)
+        print(temperatures)
